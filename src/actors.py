658574bcbefbb985613fa9a1ddcdde173962e31e
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-10-06 15:48:09 +0100

    Pass messages back to parent actor.

--- src/actors.py
+++ src/actors.py
@@ -1,8 +1,9 @@
 import logging
+import random
 from datetime import timedelta
 from time import sleep
 
-from thespian.actors import Actor, ActorSystem
+from thespian.actors import Actor, ActorSystem, ActorTypeDispatcher
 
 # https://github.com/malefs/security-smell-detector-python-gist/blob/e90764deb06ae4d3c45e702db7ad00351520348f/gist-hash/b51a9cabd41edae990fd6e844f10ef8e/snippet.py
 # https://thespianpy.com/doc/in_depth#outline-container-org9bb4305
@@ -11,19 +12,21 @@ logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 
-class BellBoy(Actor):
+class BellBoy(ActorTypeDispatcher):
     log = logging.getLogger("BellBoy")
 
-    def receiveMessage(self, message, sender):
-        self.log.info("Received message %s", message)
+    def receiveMsg_str(self, message, sender):
+        self.log.info("Received message %s from sender %s", message, sender)
         if type(message) is str and "start" in message:
             self.startBellboyServices()
         if type(message) is str and "heartbeat" in message:
             print("Got heartbeat message...")
             self.send(self.gui, "heartbeat")
             self.send(self.sensor, "heartbeat")
-        else:
-            self.wakeupAfter(timedelta(seconds=1), "wakeup")
+
+    def receiveMsg_WakeupMessage(self, message, sender):
+        self.log.info("Staying awake.")
+        self.wakeupAfter(timedelta(seconds=10))
 
     def startBellboyServices(self):
         """Starts all other BellBoy system actors."""
@@ -33,20 +36,36 @@ class BellBoy(Actor):
         self.sensor = self.createActor(Sensor)
         self.send(self.gui, "start")
         self.send(self.sensor, "start")
+        self.wakeupAfter(timedelta(seconds=1))
 
 
 class StatusWebGUI(Actor):
+    """Simple actors that inherit from Actor only need to implement recieveMessage."""
+
     log = logging.getLogger("StatusWebGUI")
 
     def receiveMessage(self, message, sender):
-        self.log.info("Received message %s", message)
+        self.log.info("Received message %s from sender %s", message, sender)
 
 
-class Sensor(Actor):
+class Sensor(ActorTypeDispatcher):
     log = logging.getLogger("Sensor")
+    parent = None
 
-    def receiveMessage(self, message, sender):
-        self.log.info("Received message %s", message)
+    def receiveMsg_str(self, message, sender):
+        self.log.info("Received message %s from sender %s", message, sender)
+        if "start" in message:
+            self.parent = sender
+            self.wakeupAfter(timedelta(seconds=3))
+
+    def receiveMsg_WakeupMessage(self, message, sender):
+        self.log.info("Checking for hand.")
+        self.wakeupAfter(timedelta(seconds=3))
+
+        # Randomly detect a hand one in four for now:
+        if random.randint(0, 100) < 25:
+            self.log.info("Got hand!")
+            self.send(self.parent, "Found a hand!")
 
 
 if __name__ == "__main__":
