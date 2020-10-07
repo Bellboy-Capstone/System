42e5162bbd6855ca3ec5077db63981b70df33cc0
Yusra Adinoyi <yozohu1@gmail.com>
2020-10-07 19:00:17 -0400

    integrated thespian actors

--- bellboy/bellboy.py
+++ bellboy/bellboy.py
@@ -3,21 +3,77 @@ import argparse
 import logging
 import os
 
+from thespian.actors import Actor, ActorSystem, ActorTypeDispatcher
+
 import sensors
 from sensors import UltrasonicSensor
 
-# cmd line argument handling
 
-parser = argparse.ArgumentParser()
-parser.add_argument('loglevel', metavar='log_level', default="INFO", nargs="?",
-                    choices=['DEBUG', 'INFO', 'WARNING', "ERROR"],
-                    help='specify level of logging (default: %(default)s)')
-args = parser.parse_args()
+class BellBoy(ActorTypeDispatcher):
+    """Lead actor. Starts other actors and co-ordinates system actions."""
+
+    log = logging.getLogger(__name__)
+    event_count = 0
+
+    # ---- MESSAGE HANDLING --- #
+    # for now all messages are strings. TO DO change that
+    def receiveMsg_str(self, message, sender):
+        """Handles string messages sent to the BellBoy actor"""
+        self.log.info("Received message %s from sender %s", message, sender)
+
+        if message == "start":
+            self.startBellboyServices()
+
+        elif "heartbeat" in message:
+            print("Got heartbeat message...")
+            self.send(self.gui, "heartbeat")
+            self.send(self.sensor, "heartbeat")
+
+        elif message == "sensorModuleRdy":
+            # sensor module ready, so request for sensors here
+            log.debug("Requesting ultrasonic sensor")
+            self.send(self.sensor_handler, "UltrasonicSensorReq")
+
+        elif "sensorReady" in message:
+            # sensor response message in form sensorReady-sensorAddress
+            # store address of the assigned sensor
+            # once weve been assigned an ultrasonic sensor,
+            # begin polling data from it, and tell it what event youre waiting on
+            self.sensor = message.split("-")[1]
+            log.debug("sending begin polling request to sensor")
+            self.send(self.sensor, "poll")
+
+        elif "sensorEventOccured" in message:
+            # once a sensorEvent happens, the message will describe the event.
+            event_count += 1
+            log.debug(str.format("event #{}: {}", event_count, message))
+
+            if event_count == 5:
+                self.send(self.sensor, "pause")
 
 
-# configures logging.
-# first method called during main, so logging configured here will be used for all modules.
+    def receiveMsg_WakeupMessage(self, message, sender):
+        self.log.info("Staying awake.")
+        self.wakeupAfter(timedelta(seconds=10))
+
+
+    # -- START SYSTEM --- #
+    def startBellboyServices(self):
+        """Starts all other BellBoy system actors."""
+        self.log.info("Starting bellboy services.")
+
+        # Create child actors. Ha.
+        # stores the address of the actor, not the actual object.
+        self.gui = self.createActor(StatusWebGUI)
+        self.sensor_handler = self.createActor(sensors.SensorModule)
+        self.send(self.sensor_handler, "init")
+
+
 def configure_logging(log_level):
+    """configures logging globally through root logger.
+    :param log_level: log verbosity
+    :type log_level: logging.LEVEL
+    """
     log_filename = "bellboy_log.txt"
     root_logger = logging.getLogger()
     root_logger.setLevel(log_level)
@@ -29,7 +85,9 @@ def configure_logging(log_level):
     ch.setLevel(log_level)
 
     # create formatter and add it to the handlers
-    formatter = logging.Formatter("%(asctime)s :: %(levelname)s :: %(name)s :: %(funcName)s() :: %(message)s")
+    formatter = logging.Formatter(
+        "%(asctime)s :: %(levelname)s :: %(name)s :: %(funcName)s() :: %(message)s"
+    )
     fh.setFormatter(formatter)
     ch.setFormatter(formatter)
 
@@ -45,22 +103,27 @@ def configure_logging(log_level):
 
 
 def main():
+    # cmd line argument handling
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "loglevel",
+        metavar="log_level",
+        default="INFO",
+        nargs="?",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR"],
+        help="specify level of logging (default: %(default)s)",
+    )
+    args = parser.parse_args()
+
     configure_logging(logging.getLevelName(args.loglevel))
-    poll_period_us = 60*1000  # 60 ms
-    sensors.init_sensor_module()
 
-    ult_sensor = UltrasonicSensor(poll_period_us)
-    try:
-        ult_sensor.init_sensor()
-        ult_sensor.begin()
-        ult_sensor.join()
+    # create multiprocessing system, housing all our actors
+    system = ActorSystem("multiprocQueueBase")
 
-    except KeyboardInterrupt:
-        pass
-
-    finally:
-        sensors.teardown_sensor_module()
+    # create lead actor
+    bellboy = system.createActor(BellBoy)
+    system.tell(bellboy, "start")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- bellboy/sensors.py
+++ bellboy/sensors.py
@@ -1,120 +1,195 @@
-# Sensor classes
-# Sensors include Camera, Ultrasonicm Mic....
 import logging
 import time
 from threading import Thread
 
-import picamera
 import RPi.GPIO as GPIO
+from thespian.actors import Actor, ActorSystem, ActorTypeDispatcher
 
 logger = logging.getLogger(__name__)
 
+# conversion factors
+US_PER_SEC = 1000000
 
-class AbstractSensor():
 
-    # constructor.
-    # creates private variables for:
-    #   the sensor's data buffer,
-    #   the polling period, and
-    #   the sensor's polling thread
-    # @param poll_period_us time inbetween sensor reads, in microseconds
-    def __init__(self, poll_period_us):
+class SensorModule(Actor):
+    """SensorModule actor handles sensors which connect to RPI.
+    This class is an interface to the sensors for whoever created it.
+    Each sensor is a thespian Actor.
+    """
 
-        self._poll_period = poll_period_us
-        self._buffer = []
+    def __init__(self):
+        self._subscriber = None  # subscriber  = Actor who this sensor module belongs to
+        self._ready = False
+
+    def init_sensor_module(self):
+        GPIO.setmode(GPIO.BOARD)  # use PHYSICAL GPIO Numbering
+        logger.debug("GPIO mode set to BOARD")
+        self._ready = True
+
+    def teardown_sensor_module(self):
+        # TO-DO: nicely kill the sensor threads.
+        GPIO.cleanup()  # use PHYSICAL GPIO Numbering
+        logger.debug("GPIO cleaned up")
+        self._ready = False
+
+    # -- MSG HANDLING --#
+    def receiveMsg(self, message, sender):
+        logger.debug(str.format("Received message {} from {}", message, sender))
+
+        switcher = {
+            "init": receiveMsg_init,
+            "teardown": receiveMsg_teardown,
+            "UltrasonicSensorReq": receiveMsg_UltrasonicSensorReq,
+        }
+
+        func = switcher.get(message, receiveMsg_unknown)
+
+    def receiveMsg_init(self, message, sender):
+        self._subscriber = sender
+        self.init_sensor_module()
+        self.send(sender, "sensorModuleRdy")
+
+    def receiveMsg_teardown(self, message, sender):
+        if not self._ready:
+            logger.warning(
+                str.format("nothing to teardown, sensor module not initialized")
+            )
+
+        elif sender != self._subscriber:
+            logger.warning(
+                str.format(
+                    "teardown request from unauthorized sender {}, ignored", sender
+                )
+            )
+
+        else:
+            self.teardown_sensor_module()
+            self._ready = False
+
+    def receiveMsg_UltrasonicSensorReq(self, message, sender):
+        """an actor has requested an ultrasonic sensor.
+        creates the sensor and send init msg, indicating the actor who requested it/ will be subscribed to it
+        """
+        sensor = self.createActor(UltrasonicSensor)
+        self.send(sensor, "init-" + sender)
+
+    def receiveMsg_unknown(self, message, sender):
+        logger.warning("received unknown message: " + message)
+
+
+# sensor blueprint
+class AbstractSensor(Actor):
+    def __init__(self):
         super().__init__()
-        self._sensor_thread = Thread(target=self._read_sensor_loop)
+        self._poll_period = 100000  # 100 ms
+        self._buffer = []
+        self._sensor_thread = Thread(target=self._sensor_loop)
+        self._terminate_thread = True
 
     # setup the sensor
     def init_sensor(self):
-        pass
+        logger.error(str.format("%(funcName)s not implemented!"))
 
     # close the sensor
     def close_sensor(self):
-        pass
+        logger.error(str.format("%(funcName)s not implemented!"))
 
-    # string rep of sensors current status
-    def brief_status(self):
-        pass
+    # loop run in sensor's polling thread
+    def _sensor_loop(self):
+        logger.error(str.format("%(funcName)s not implemented!"))
 
-    # loop to be run in sensor's thread
-    def _read_sensor_loop(self):
-        # wait for self._poll_period
-        pass
-
-    def join(self):
-        self._sensor_thread.join()
+    # def join(self):
+    #     self._sensor_thread.join()
 
     def begin(self):
-        logger.debug("starting ultrasonic sensor thread")
+        self._terminate_thread = False
+        logger.debug("starting sensor's thread")
         self._sensor_thread.start()
 
-
-class TestSensor(AbstractSensor):
-    def __init__(self):
-        super().__init__(300)
-
-
-class RPICamSensor(AbstractSensor):
-    def __init__(self, poll_period_us):
-        super().__init__(poll_period_us)
-        self.picam: PiCamera = PiCamera()
-
-    def init_sensor(self):
-        pass
-
-    def close_sensor(self):
-        pass
-
-    def brief_status(self):
-        pass
-
-    def read_sensor(self):
-        pass
+    def receiveMsg(self, message, sender):
+        logger.error(str.format("%(funcName)s not implemented!"))
 
 
 class UltrasonicSensor(AbstractSensor):
-    def __init__(self, poll_period_us):
-        super().__init__(poll_period_us)
-
-    # setup the sensor
-    def init_sensor(self, trigPin=16, echoPin=18, max_depth_cm=200, pulse_width_us=10):
+    def init_sensor(
+        self,
+        ultrasonic_event_func=average_distance_lt20,
+        poll_period_us=100000,
+        trigPin=16,
+        echoPin=18,
+        max_depth_cm=200,
+        pulse_width_us=10,
+    ):
+        """
+        :param poll_period_us: read period of sensor in microseconds
+        :type poll_period_us: float
+        :param ultrasonic_event_func: function characterizing the event the sensor will listen for.
+                                      this function analyzes a data buffer,
+                                      and returns the Event which occured, if any.
+        :type ultrasonic_event_func: func(arr), returns Event
+        """
+        self._poll_period = poll_period_us
+        self._event_occured = ultrasonic_event_func
         self._trigPin = trigPin
         self._echoPin = echoPin
         self._max_depth_cm = max_depth_cm
         self._pulse_width = pulse_width_us
-        self._time_out = max_depth_cm*60*0.000001
+        self._time_out = max_depth_cm * 60 * 0.000001
 
-        GPIO.setup(trigPin, GPIO.OUT)   # set trigPin to OUTPUT mode
-        GPIO.setup(echoPin, GPIO.IN)    # set echoPin to INPUT mode
-
-    # close the sensor
+        GPIO.setup(trigPin, GPIO.OUT)  # set trigPin to OUTPUT mode
+        GPIO.setup(echoPin, GPIO.IN)  # set echoPin to INPUT mode
 
     def close_sensor(self):
-        GPIO.cleanup()         # release GPIO resource
+        GPIO.cleanup()  # release GPIO resource
 
-    # string rep of sensors current status
-    def brief_status(self):
-        pass
-
-    # read sensor once.
-    # to be called continuously in sensor's thread
-    def _read_sensor_loop(self):
-        # send high pulse
+    def _sensor_loop(self):
+        """sensor loop, every period it:
+        - stores a depth reading,
+        - analyzes recent readings to test for an event
+        until thread terminate flag is raised.
+        """
         global logger
-        while True:
+
+        while not self._terminate_thread:
+
+            # send ultrasonic ping
             t0 = time.time()
             GPIO.output(self._trigPin, GPIO.HIGH)
-            time.sleep(self._pulse_width/1000000.0)  # us --> sec
-            GPIO.output(self._trigPin, GPIO.LOW)  # make trigPin output LOW level
-            pingTime = self.pulseIn(self._echoPin, GPIO.HIGH, self._time_out)   # read plus time of echoPin
+            time.sleep(self._pulse_width / US_PER_SEC)
+            GPIO.output(self._trigPin, GPIO.LOW)
+
+            # calculate distance from reflected ping
+            pingTime = self.pulseIn(self._echoPin, GPIO.HIGH, self._time_out)
             distance = pingTime * 340.0 / 2.0 / 10000.0
             self._buffer.append(distance)
-            logger.debug(str.format("Ultrasonic depth recorded: {}", self._buffer[len(self._buffer)-1]))
-            dt_us = (time.time() - t0) * 1000000
-            time.sleep((self._poll_period - dt_us)/1000000)
+            logger.debug(
+                str.format(
+                    "Ultrasonic depth recorded: {}", self._buffer[len(self._buffer) - 1]
+                )
+            )
+
+            # check for event, if occurred send msg to subscriber
+            event = self._event_occured(self._buffer)
+            if event:
+                self.send(subscriber, event)
+
+            # sleep till next period
+            dt_sec = time.time() - t0  # TO-DO: ensure dt_sec < poll_period
+            time.sleep(self._poll_period / US_PER_SEC - dt_sec)
+        logger.info("polling thread terminated")
 
     def pulseIn(self, pin, level, timeOut):  # obtain pulse time of a pin under timeOut
+        """ultrasonic ranging code copy pasted
+
+        :param pin: [description]
+        :type pin: [type]
+        :param level: [description]
+        :type level: [type]
+        :param timeOut: [description]
+        :type timeOut: [type]
+        :return: [description]
+        :rtype: [type]
+        """
         t0 = time.time()
         while GPIO.input(pin) != level:
             if (time.time() - t0) > self._time_out:
@@ -128,18 +203,48 @@ class UltrasonicSensor(AbstractSensor):
         pulseTime = (time.time() - t0) * 1000000
         return pulseTime
 
+    def receiveMsg(self, message, sender):
+        """message handler. all msgs are strings for now, so only this method is needed"""
 
-class MicrophoneSensor(AbstractSensor):
-    def __init__(self, poll_period_us):
-        super().__init__(poll_period_us)
+        # init message comes in form "init-subscriber"
+        if "init" in message:
+            self.init_sensor()
+            self.subscriber = message.split("-")[1]
+            self.send(self.subscriber, "sensorReady-" + self.Addr)
+
+        elif "poll" in message:
+            self.begin()
+
+        elif "pause" in message:
+            self._terminate_thread = True
 
 
-def init_sensor_module():
-    GPIO.setmode(GPIO.BOARD)      # use PHYSICAL GPIO Numbering
-    logger.debug("GPIO mode set to BOARD")
+# -- EXAMPLE EVENT FUNCTIONS ---#
+# these functions analyze an array of data and
+# returns the Event which occured, None if none if it didnt happen
 
 
-def teardown_sensor_module():
-    # TO-DO: nicely kill the sensor threads.
-    GPIO.cleanup()      # use PHYSICAL GPIO Numbering
-    logger.debug("GPIO cleaned up")
+def average_distance_lt20(distance_array):
+    sum = 0.0
+    for distance in distance_array[-5:]:
+        if distance == 0.0:
+            return None
+
+        sum += distance
+
+    if sum / 5 < 20:
+        return str.format(
+            "sensorEventOccurred: average distance was less than 20: {}", sum / 5
+        )
+
+    return None
+
+
+def buttonXHovered(distance_array):
+    # lets say button is within distances 10 - 15.
+    sum = 0
+    for distance in distance_array[-5:]:
+        if distance > 15 or distance < 10:
+            return None
+
+    return "sensorEventOccurred: buttonX was hovered"
