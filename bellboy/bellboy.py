cc2ee833ad70a80ceeb549413cefc7148b342b54
Yusra Adinoyi <yozohu1@gmail.com>
2020-10-07 23:37:48 -0400

    fixed errors abt thespian stuff

--- bellboy/bellboy.py
+++ bellboy/bellboy.py
@@ -12,17 +12,20 @@ from sensors import UltrasonicSensor
 class BellBoy(ActorTypeDispatcher):
     """Lead actor. Starts other actors and co-ordinates system actions."""
 
-    log = logging.getLogger(__name__)
+    log = logging.getLogger("BellBoy")
     event_count = 0
 
     # ---- MESSAGE HANDLING --- #
-    # for now all messages are strings. TO DO change that
+    # TO DO class messages instead of strings/dicts
     def receiveMsg_str(self, message, sender):
         """Handles string messages sent to the BellBoy actor"""
         self.log.info("Received message %s from sender %s", message, sender)
 
         if message == "start":
             self.startBellboyServices()
+
+        if message == "end":
+            self.endBellboyServices()
 
         elif "heartbeat" in message:
             print("Got heartbeat message...")
@@ -31,42 +34,58 @@ class BellBoy(ActorTypeDispatcher):
 
         elif message == "sensorModuleRdy":
             # sensor module ready, so request for sensors here
-            log.debug("Requesting ultrasonic sensor")
+            self.log.debug("Requesting ultrasonic sensor")
             self.send(self.sensor_handler, "UltrasonicSensorReq")
 
-        elif "sensorReady" in message:
+    def receiveMsg_dict(self, message, sender):
+        if message["type"] == "sensorReady":
             # sensor response message in form sensorReady-sensorAddress
             # store address of the assigned sensor
             # once weve been assigned an ultrasonic sensor,
             # begin polling data from it, and tell it what event youre waiting on
-            self.sensor = message.split("-")[1]
-            log.debug("sending begin polling request to sensor")
+            self.sensor = message["sensorAddr"]
+            self.log.debug("sending begin polling request to sensor")
             self.send(self.sensor, "poll")
 
-        elif "sensorEventOccured" in message:
-            # once a sensorEvent happens, the message will describe the event.
-            event_count += 1
-            log.debug(str.format("event #{}: {}", event_count, message))
+        if message["type"] == "sensorEventOccured":
+            self.event_count += 1
+            self.log.info(str.format("event #{}: {}", self.event_count, message["payload"]))
 
-            if event_count == 5:
+            if self.event_count == 5:
+                self.log.info("received 5 events, pausing sensor thread")
                 self.send(self.sensor, "pause")
-
+                #self.send(self.sensor_handler, "teardown")
 
     def receiveMsg_WakeupMessage(self, message, sender):
         self.log.info("Staying awake.")
         self.wakeupAfter(timedelta(seconds=10))
 
-
-    # -- START SYSTEM --- #
+    # -- START / END SYSTEM -- #
     def startBellboyServices(self):
         """Starts all other BellBoy system actors."""
         self.log.info("Starting bellboy services.")
 
         # Create child actors. Ha.
-        # stores the address of the actor, not the actual object.
         self.gui = self.createActor(StatusWebGUI)
         self.sensor_handler = self.createActor(sensors.SensorModule)
         self.send(self.sensor_handler, "init")
+
+    def endBellboyServices(self):
+        self.log.info("Terminating bellboy services.")
+        self.send(self.sensor_handler, "teardown")
+
+
+class StatusWebGUI(Actor):
+    """
+    Will eventually deploy a simple Flask site as a simple frontend for the device.
+    Simple actors that inherit from Actor only need to implement recieveMessage.
+    """
+
+    log = logging.getLogger("StatusWebGUI")
+
+    def receiveMessage(self, message, sender):
+        """Handles all messages sent to the StatusWebGUI actor"""
+        self.log.info("Received message %s from sender %s", message, sender)
 
 
 def configure_logging(log_level):
@@ -122,7 +141,13 @@ def main():
 
     # create lead actor
     bellboy = system.createActor(BellBoy)
-    system.tell(bellboy, "start")
+    try:
+        system.tell(bellboy, "start")
+        system.listen(timeout=1*60)  # automatically ends after a minute
+    except KeyboardInterrupt:
+        pass
+    finally:
+        system.tell(bellboy, "end")
 
 
 if __name__ == "__main__":
--- bellboy/sensors.py
+++ bellboy/sensors.py
@@ -2,6 +2,8 @@ import logging
 import time
 from threading import Thread
 
+from UltrasonicRanging import pulseIn
+
 import RPi.GPIO as GPIO
 from thespian.actors import Actor, ActorSystem, ActorTypeDispatcher
 
@@ -13,50 +15,59 @@ US_PER_SEC = 1000000
 
 class SensorModule(Actor):
     """SensorModule actor handles sensors which connect to RPI.
-    This class is an interface to the sensors for whoever created it.
-    Each sensor is a thespian Actor.
+       Sensors can only be created through a request to this module
     """
-
+    logger = logging.getLogger("SensorModule")
     def __init__(self):
-        self._subscriber = None  # subscriber  = Actor who this sensor module belongs to
         self._ready = False
+        self.sensors = []
 
     def init_sensor_module(self):
+        self.logger.info("initializing sensor module")
         GPIO.setmode(GPIO.BOARD)  # use PHYSICAL GPIO Numbering
-        logger.debug("GPIO mode set to BOARD")
+        self.logger.debug("GPIO mode set to BOARD")
         self._ready = True
 
     def teardown_sensor_module(self):
-        # TO-DO: nicely kill the sensor threads.
-        GPIO.cleanup()  # use PHYSICAL GPIO Numbering
-        logger.debug("GPIO cleaned up")
+        self.logger.info("tearing down sensor module")
+
+        # end any running threads
+        self.logger.debug("ending sensor threads ...")
+        for sensor in self.sensors:
+            self.send(sensor, "pause")
+
+        GPIO.cleanup()
+        self.logger.debug("GPIO cleaned up")
         self._ready = False
 
-    # -- MSG HANDLING --#
-    def receiveMsg(self, message, sender):
-        logger.debug(str.format("Received message {} from {}", message, sender))
+    # -- MSG HANDLING -- #
+    def receiveMessage(self, message, sender):
+        self.logger.debug(str.format("Received message {} from {}", message, sender))
 
         switcher = {
-            "init": receiveMsg_init,
-            "teardown": receiveMsg_teardown,
-            "UltrasonicSensorReq": receiveMsg_UltrasonicSensorReq,
+            "init": self.receiveMsg_init,
+            "teardown": self.receiveMsg_teardown,
+            "UltrasonicSensorReq": self.receiveMsg_UltrasonicSensorReq,
         }
 
-        func = switcher.get(message, receiveMsg_unknown)
+        func = switcher.get(message, self.receiveMsg_unknown)
+        func(message, sender)
 
     def receiveMsg_init(self, message, sender):
+        self.logger.debug("in")
         self._subscriber = sender
         self.init_sensor_module()
         self.send(sender, "sensorModuleRdy")
+        self.logger.debug(str.format("sent ready message to {}", sender))
 
     def receiveMsg_teardown(self, message, sender):
         if not self._ready:
-            logger.warning(
+            self.logger.warning(
                 str.format("nothing to teardown, sensor module not initialized")
             )
 
         elif sender != self._subscriber:
-            logger.warning(
+            self.logger.warning(
                 str.format(
                     "teardown request from unauthorized sender {}, ignored", sender
                 )
@@ -70,15 +81,47 @@ class SensorModule(Actor):
         """an actor has requested an ultrasonic sensor.
         creates the sensor and send init msg, indicating the actor who requested it/ will be subscribed to it
         """
+        if not self._ready:
+            log.warning("sensor module not ready!")
+            return
+        self.logger.debug(str.format("creating UltrasonicSensor for {}", sender))
         sensor = self.createActor(UltrasonicSensor)
-        self.send(sensor, "init-" + sender)
+        self.sensors.append(sensor)
+        self.send(sensor, {"type": "init", "subscriber": sender})
 
     def receiveMsg_unknown(self, message, sender):
-        logger.warning("received unknown message: " + message)
+        self.logger.warning("received unknown message: " + message)
+
+# -- EXAMPLE EVENT FUNCTIONS ---#
+# these functions analyze an array of data and
+# returns the Event which occured as a message, if it happened
+
+
+def average_distance_lt20(distance_array):
+    sum = 0.0
+    for distance in distance_array[-5:]:
+        if distance == 0.0:
+            return None
+
+        sum += distance
+
+    if sum / 5 < 20:
+        return {"type": "sensorEventOccured", "payload": str.format("sensorEventOccurred: average distance was less than 20: {}", sum / 5)}
+    return None
+
+
+def buttonXHovered(distance_array):
+    # lets say button is within distances 10 - 15.
+    sum = 0
+    for distance in distance_array[-5:]:
+        if distance > 15 or distance < 10:
+            return None
+
+    return {"type": "sensorEventOccured", "payload": str.format("sensorEventOccurred: button at 12.5 cm was hovered")}
 
 
 # sensor blueprint
-class AbstractSensor(Actor):
+class AbstractSensor(ActorTypeDispatcher):
     def __init__(self):
         super().__init__()
         self._poll_period = 100000  # 100 ms
@@ -103,7 +146,7 @@ class AbstractSensor(Actor):
 
     def begin(self):
         self._terminate_thread = False
-        logger.debug("starting sensor's thread")
+        logger.info("starting sensor's thread")
         self._sensor_thread.start()
 
     def receiveMsg(self, message, sender):
@@ -111,9 +154,12 @@ class AbstractSensor(Actor):
 
 
 class UltrasonicSensor(AbstractSensor):
+
+    logger = logging.getLogger("UltrasonicSensor")
+
     def init_sensor(
         self,
-        ultrasonic_event_func=average_distance_lt20,
+        ultrasonic_event_func=buttonXHovered,
         poll_period_us=100000,
         trigPin=16,
         echoPin=18,
@@ -139,12 +185,9 @@ class UltrasonicSensor(AbstractSensor):
         GPIO.setup(trigPin, GPIO.OUT)  # set trigPin to OUTPUT mode
         GPIO.setup(echoPin, GPIO.IN)  # set echoPin to INPUT mode
 
-    def close_sensor(self):
-        GPIO.cleanup()  # release GPIO resource
-
     def _sensor_loop(self):
         """sensor loop, every period it:
-        - stores a depth reading,
+        - stores a depth reading, and
         - analyzes recent readings to test for an event
         until thread terminate flag is raised.
         """
@@ -159,10 +202,10 @@ class UltrasonicSensor(AbstractSensor):
             GPIO.output(self._trigPin, GPIO.LOW)
 
             # calculate distance from reflected ping
-            pingTime = self.pulseIn(self._echoPin, GPIO.HIGH, self._time_out)
+            pingTime = pulseIn(self._echoPin, GPIO.HIGH, self._time_out)
             distance = pingTime * 340.0 / 2.0 / 10000.0
             self._buffer.append(distance)
-            logger.debug(
+            self.logger.debug(
                 str.format(
                     "Ultrasonic depth recorded: {}", self._buffer[len(self._buffer) - 1]
                 )
@@ -171,80 +214,27 @@ class UltrasonicSensor(AbstractSensor):
             # check for event, if occurred send msg to subscriber
             event = self._event_occured(self._buffer)
             if event:
-                self.send(subscriber, event)
+                self.send(self.subscriber, event)
 
             # sleep till next period
             dt_sec = time.time() - t0  # TO-DO: ensure dt_sec < poll_period
             time.sleep(self._poll_period / US_PER_SEC - dt_sec)
-        logger.info("polling thread terminated")
+        self.logger.info("polling thread terminated")
 
-    def pulseIn(self, pin, level, timeOut):  # obtain pulse time of a pin under timeOut
-        """ultrasonic ranging code copy pasted
+    def receiveMsg_str(self, message, sender):
+        """most messages are strings for now """
 
-        :param pin: [description]
-        :type pin: [type]
-        :param level: [description]
-        :type level: [type]
-        :param timeOut: [description]
-        :type timeOut: [type]
-        :return: [description]
-        :rtype: [type]
-        """
-        t0 = time.time()
-        while GPIO.input(pin) != level:
-            if (time.time() - t0) > self._time_out:
-                return 0
-
-        t0 = time.time()
-        while GPIO.input(pin) == level:
-            if (time.time() - t0) > self._time_out:
-                return 0
-
-        pulseTime = (time.time() - t0) * 1000000
-        return pulseTime
-
-    def receiveMsg(self, message, sender):
-        """message handler. all msgs are strings for now, so only this method is needed"""
-
-        # init message comes in form "init-subscriber"
-        if "init" in message:
-            self.init_sensor()
-            self.subscriber = message.split("-")[1]
-            self.send(self.subscriber, "sensorReady-" + self.Addr)
-
-        elif "poll" in message:
+        logger.info(str.format("Received message {} from {}", message, sender))
+        if "poll" in message:
             self.begin()
 
         elif "pause" in message:
             self._terminate_thread = True
 
+    def receiveMsg_dict(self, message, sender):
+        """message handler for more involved messages"""
 
-# -- EXAMPLE EVENT FUNCTIONS ---#
-# these functions analyze an array of data and
-# returns the Event which occured, None if none if it didnt happen
-
-
-def average_distance_lt20(distance_array):
-    sum = 0.0
-    for distance in distance_array[-5:]:
-        if distance == 0.0:
-            return None
-
-        sum += distance
-
-    if sum / 5 < 20:
-        return str.format(
-            "sensorEventOccurred: average distance was less than 20: {}", sum / 5
-        )
-
-    return None
-
-
-def buttonXHovered(distance_array):
-    # lets say button is within distances 10 - 15.
-    sum = 0
-    for distance in distance_array[-5:]:
-        if distance > 15 or distance < 10:
-            return None
-
-    return "sensorEventOccurred: buttonX was hovered"
+        if message["type"] == "init":
+            self.init_sensor()
+            self.subscriber = message["subscriber"]
+            self.send(self.subscriber, {"type": "sensorReady", "sensorAddr": self.myAddress})
