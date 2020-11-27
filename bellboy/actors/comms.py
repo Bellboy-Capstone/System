5d61dd31575af5e132160f2b2ca3f8acea4e478c
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-11-27 04:15:56 +0000

    Successfully save ID when starting bellboy.

--- bellboy/actors/comms.py
+++ bellboy/actors/comms.py
@@ -1,9 +1,13 @@
+import os
+import pickle
+
 import requests
 from actors.generic import GenericActor
 from utils.messages import CommsReq, CommsResp, Response
 
 
 endpoint = "https://bellboy-services.herokuapp.com"
+credential_path = "bellboy-credentials.obj"
 
 
 class CommsActor(GenericActor):
@@ -25,11 +29,41 @@ class CommsActor(GenericActor):
             self.log.error("Services are not up.")
             raise Exception("Services are down?")
 
-        if self._authenticated is False and self._identifier is None:
-            self.log.info("Authenticating with Services, getting new ID.")
+        if self._authenticated is False or self._identifier is None:
+            # Unpickle the auth file if it is present:
+            if (
+                os.path.exists(credential_path)
+                and os.path.isfile(credential_path)
+                and os.path.getsize(credential_path) > 0
+            ):
+                self.log.info("Found credential file, unpickling...")
+                auth_file = open(credential_path, "rb")
+                self._identifier = str(pickle.load(auth_file))
+                auth_file.close()
+                self._authenticated = True
+            else:
+                # If no file is present, we need to fetch credentials from Services.
+                self.log.info(
+                    "No saved credentials. Registering device with Services..."
+                )
+                auth_req = requests.get(f"{endpoint}/bellboy/register-device/")
+                data = auth_req.json()
+                self.log.debug("Authorization result: %s", data)
+                if req.status_code == 200:
+                    self.log.debug("New ID is %s", data.get("identifier"))
+                else:
+                    raise Exception("Could not authenticate!")
 
-        else:
-            self.log.info("Already authenticated.")
+                # Save that identifier for use now:
+                self._identifier = str(data.get("identifier"))
+                self._authenticated = True
+
+                # Now we've got a new identifier, so let's pickle it for storage:
+                new_auth_file = open(credential_path, "wb")
+                pickle.dump(self._identifier, new_auth_file)
+                new_auth_file.close()
+
+        self.log.info("Authenticated, ID is %s", self._identifier)
 
     # --------------------------#
     # MESSAGE HANDLING METHODS  #
