1298acc57ebc5f75de469ad6e0f2683582d4a802
Yusra Adinoyi <yozohu1@gmail.com>
2021-03-28 20:00:28 -0400

    oled stuff

---
+++ experiments/speech/gif2oled.py
@@ -0,0 +1,21 @@
+# gif to oled
+
+# 1 extract frames
+
+from PIL import Image, ImageSequence
+
+
+gif_path = "audio_animation.gif"
+gif_name = gif_path.split('.')[0]
+
+gif = Image.open(gif_path)
+frames = []
+for frame in ImageSequence.Iterator(gif):
+    frames.append(frame)
+
+
+# get the colour info 
+
+# convert to hex format
+
+# save?
